e17bd017dcda41ce297d4db8c7979ec915c7a2d5
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-10-30 03:53:54 +0900

    Add migrate script

--- background/launch.sh
+++ background/launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sleep 10
+sleep 15
 
 cd notifier
+go install -tags 'mysql' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+migrate -database 'mysql://root:r00t-not1f1er@tcp(db:3306)/notifier?parseTime=true' -path db up
 go run main.go
