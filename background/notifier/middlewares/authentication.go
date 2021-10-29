7ff5ebaede8d2216827b68edfb9ebebf19a60fa0
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-10-30 03:10:41 +0900

    Add new jwt

--- background/notifier/middlewares/authentication.go
+++ background/notifier/middlewares/authentication.go
@@ -1,20 +1,30 @@
 package middlewares
 
 import (
+	"background/notifier/aop"
 	"context"
-	"errors"
+	"github.com/golang-jwt/jwt"
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 )
 
 func Authenticate(ctx context.Context) (context.Context, error) {
-	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	tokenString, err := grpc_auth.AuthFromMD(ctx, "Bearer")
 	if err != nil {
 		return nil, err
 	}
 
-	if token != "testToken" {
-		return nil, errors.New("unauthorized")
-	}
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, err
+		}
 
-	return ctx, nil
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return aop.HmacSecret, nil
+	})
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return ctx, nil
+	} else {
+		return nil, err
+	}
 }
