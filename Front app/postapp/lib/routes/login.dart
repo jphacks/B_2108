13ad934ed3b030ad33824d0bd20badf8d5a40229
RosesAndViolets <61640805+RosesAndViolets@users.noreply.github.com>
2021-10-30 10:19:16 +0900

    writing login page authentication

--- Front app/postapp/lib/routes/login.dart
+++ Front app/postapp/lib/routes/login.dart
@@ -10,42 +10,67 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController pwdController = TextEditingController();
 
   String _message = '';
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text('Login Form')),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: unameController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: 'User name',
-                    hintText: 'rcc@example.com',
-                    icon: Icon(Icons.email_outlined),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: unameController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: 'User name',
-                    hintText: 'rcc@example.com',
-                    icon: Icon(Icons.email_outlined),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            )
-          ],
-        ),
+      appBar: AppBar(title: const Text('Login Form')),
+      body: Center(
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: unameController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        labelText: 'User name',
+                        hintText: 'rcc@example.com',
+                        icon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.zero)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: pwdController,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: 'password',
+                        hintText: 'Enter your password',
+                        icon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.zero)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        var uname = unameController.text;
+                        var pwd = pwdController.text;
+                        setState(() {
+                          _message = "User name : $uname\nPassword : ";
+                        });
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(_message),
+                ),
+              ],
+            )),
       ),
     ));
   }
