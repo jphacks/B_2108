78ac9d89fb62f8ddcd08425632b2f39cd762e4e3
kenboo0426 <kenshin.kenboo@gmail.com>
2021-10-30 12:49:01 +0900

    cancel next button remove

--- Front app/postapp/lib/widgets/inbox.dart
+++ Front app/postapp/lib/widgets/inbox.dart
@@ -10,20 +10,18 @@ class Inbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stepper(
+      controlsBuilder: (BuildContext context,
+          {VoidCallback? onStepContinue, VoidCallback? onStepCancel}) {
+        return Row();
+      },
       steps: <Step>[
         Step(
-          title: const Text('Step 1 title'),
-          content: Container(
-              alignment: Alignment.centerLeft,
-              child: const Text('Content for Step 1')),
+          title: Text('${created_at}にポストされました'),
+          content: Text(""),
         ),
         Step(
-          title: Text('Step 2 title'),
-          content: Text('Content for Step 2'),
-        ),
-        Step(
-          title: Text('Step 3 title'),
-          content: Text('Content for Step 3'),
+          title: Text('${created_at}にポストされました'),
+          content: Text(""),
         ),
       ],
     );
